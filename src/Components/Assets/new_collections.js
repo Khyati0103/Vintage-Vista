fbfbfede0c45af4bfd4afbc27c82b7c111e52f83
Khyati0103 <khyatinirajsingh@gmail.com>
2024-12-05 23:39:18 +0530

    Updated submodule content

---
+++ src/Components/Assets/new_collections.js
@@ -0,0 +1,68 @@
+import p1_img from "./product_12.png";
+import p2_img from "./product_5.png";
+import p3_img from "./product_14.png";
+import p4_img from "./product_8.png";
+import p5_img from "./product_18.png";
+import p6_img from "./product_27.png";
+import p7_img from "./product_17.png";
+import p8_img from "./product_30.png";
+
+let new_collections = [
+    {
+        id: 12,
+        name: "Blue Cotton Fine Decorative Mirror",
+        category: "mirror",
+        image: p1_img,
+        price: 849,
+    },
+    {
+        id: 5,
+        name: "Small Aloe Vera",
+        category: "plants",
+        image: p2_img,        
+        price: 400,
+    },
+    {
+        id: 14,
+        name: "Pink Cotton Imperial Decorative Mirror",
+        category: "mirror",
+        image: p3_img,
+        price: 849,
+    },
+    {
+        id: 8,
+        name: "Caladium Sweethearts",
+        category: "plants",
+        image: p4_img,
+        price: 2200,
+    },
+    {
+        id: 18,
+        name: "Floral rangoli mat, Indian Decor",
+        category: "festive",
+        image: p5_img,
+        price: 1199,
+    },
+    {
+        id: 27,
+        name: "Dancing Women Warli Art Wall Painting",
+        category: "painting",
+        image: p6_img,
+        price: 2999,
+    },
+    {
+        id: 17,
+        name: "37 Inches Shub Labh Toran",
+        category: "festive",
+        image: p7_img,
+        price: 749,
+    },
+    {
+        id: 30,
+        name: "Canvas Painting Beautiful Nature Art",
+        category: "painting",
+        image: p8_img,
+        price: 1999,
+    },
+];
+export default new_collections
